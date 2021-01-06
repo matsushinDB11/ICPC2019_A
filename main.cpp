a5abd7f3232bc85478fb6a383736378660f16fff
matsushinDB11 <matsushin35@gmail.com>
2021-01-06 12:04:03 +0900

    fin 2021-01-05

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@
  */
 
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int sum_of_array(int x[]){
+int sum_of_array(int x[], int size_of_array){
     int sum = 0;
-    int size_of_array = sizeof(x) / sizeof(x[0]);
+//    int size_of_array = sizeof(x) / sizeof(x[0]);
     for (int i = 0; i < size_of_array; ++i) {
         sum += x[i];
     }
@@ -31,10 +32,12 @@ int main() {
                 cin >> points_array[j];
             }
             // 各生徒の合計点
-            int sum_of_point = sum_of_array(points_array);
+            int sum_of_point = sum_of_array(points_array, num_of_subjects);
             total_score_of_each_student[i] = sum_of_point;
             cout << sum_of_point << endl;
         }
+                int max_sum_of_point = *max_element(total_score_of_each_student, total_score_of_each_student + num_of_students);
+                cout << "*" << max_sum_of_point << endl;
     }
     return 0;
 }
