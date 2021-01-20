d1c26f66949001ad6fded885def4bccb43f2e272
Shintaro Matsumoto <matsushin35@gmail.com>
2021-01-20 19:02:54 +0900

    add comment

--- main.cpp
+++ main.cpp
@@ -12,20 +12,23 @@ using namespace std;
 
 int main() {
     while (true) {
+        // 生徒数, 教科数
         int num_of_students, num_of_subjects;
         cin >> num_of_students;
         cin >> num_of_subjects;
         // 入力が2つの0だった場合終了
         if (num_of_students == 0 && num_of_subjects == 0) break;
+        // 得点格納用二次元配列
         int points_array[num_of_subjects][num_of_students];
+        // 各教科の得点を二次元配列に入力
         for (int i = 0; i < num_of_subjects; ++i) {
             for (int j = 0; j < num_of_students; ++j) {
-                // 各教科の得点を入力
                 cin >> points_array[i][j];
             }
         }
-        // 各生徒の合計点を集計
+        // 各生徒の合計点
         int sum_of_points[num_of_students];
+        // 各生徒の合計点を集計
         for (int j = 0; j < num_of_students; ++j) {
             int sum_of_point = 0;
             for  (int i = 0; i < num_of_subjects; ++i) {
@@ -33,6 +36,7 @@ int main() {
             }
             sum_of_points[j] = sum_of_point;
         }
+        // 最も合計点が高い生徒の合計点
         int max_sum_of_point = max_of_array(sum_of_points, num_of_students);
         cout << max_sum_of_point << endl;
     }
